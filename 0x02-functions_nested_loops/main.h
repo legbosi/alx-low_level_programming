132a28977260e2933277c246932f64d265a98531
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-07-07 16:31:57 -0700

    digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,13 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
 
 #endif
