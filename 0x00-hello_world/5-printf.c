bef6f04de913896b49dc7614034b3e6e59b8d870
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-07-14 10:24:30 -0700

    betty babe

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints 'with proper grammar but the ouptu is a piece of art'
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description:'Write a C program that prints exact
+ * Return:Always 0 (Success)
  */
 
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art, \n)
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
