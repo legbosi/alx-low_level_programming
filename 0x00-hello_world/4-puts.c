fb7e946d3e6d27247ec38f37cffe50a12d6157d9
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-07-14 10:15:41 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Write 'Programming is like building a multilingual puzzle'
- * Return: Always 0 (success)
+ * main - Entry point
+ * Description:'Write a C program that prints exact
+ * Return:Always 0 (Success)
  */
+
 int main(void)
 {
-	puts('Programming is like building a multilingual puzzle')
+
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
+
+
