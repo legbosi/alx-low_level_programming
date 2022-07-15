b9d2a16cd0c750dd44f16eca629b3d7bbfbbd494
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-07-15 15:03:33 -0700

    starting the process

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,12 +2,13 @@
 
 /**
  * _strncat - concatenates two strings
+ *
  * @src: The source of strings
  * @dest: The destination of the string
  * @n: The length of int
+ *
  * Return: pointer to the resulting string dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
@@ -19,6 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
-		dest[i + j] = '\0';
-		return (dest);
 	}
+	dest[i + j] = '\0';
+	return (dest);
+}
