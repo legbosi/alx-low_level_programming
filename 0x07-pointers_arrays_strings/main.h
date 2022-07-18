fc0ab0b8ac9ec76d94082b9171cc1e3fae6a12ae
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-07-18 10:47:06 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -13,5 +14,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif/*MAIN_H*/
