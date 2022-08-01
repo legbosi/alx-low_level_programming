ebe10e266a0e93de2e2f66536de480e01b609e6b
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-08-01 15:06:50 -0700

    the preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point, prints file name it was compiled from
+ *
+ *    Return: 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+		printf("%s\n", __FILE__);
+
+		return (0);
+
+}
