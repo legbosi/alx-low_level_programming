8fe62adfca13cf6d15107e3e9e90cbb263576592
Iyiibe Ann Legbosi <iyiibeanna@gmail.com>
2022-08-16 16:15:36 -0700

    reverse listint

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,71 +1,28 @@
-
-
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list
+ * reverse_listint - reverses a linked list.
  * @head: head of a list.
  *
- * Return: no return.
+ * Return: pointer to the first node.
  */
-void free_listp(listp_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
-	listp_t *temp;
-	listp_t *curr;
+	listint_t *p;
+	listint_t *n;
 
-	if (head != NULL)
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
+
+	*head = p;
+	return (*head);
 }
-
-/**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-
-	hptr = NULL;
-	while (head != NULL)
-	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (nnodes);
-			}
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nnodes++;
-	}
-
-	free_listp(&hptr);
-	return (nnodes);
 
